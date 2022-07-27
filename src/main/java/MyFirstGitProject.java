969ebcf838cc30c663b943b0712964093af72c67
PavelKaminski <sdminvest@gmail.com>
2022-07-27 23:55:29 +0300

    1.1

--- src/main/java/MyFirstGitProject.java
+++ src/main/java/MyFirstGitProject.java
@@ -2,6 +2,6 @@ public class MyFirstGitProject {
     public static void main(String[] args) {
 
         System.out.println("Hello, Git World");
-        System.out.println("Hello again!");
+        System.out.println("Hello again and again!");
     }
 }
